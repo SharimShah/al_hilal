88702ee65cf7bb1ed2a5d9e9d9ae9010f726052c
SharimShah <fsd1shah@gmail.com>
2025-09-19 14:40:52 +0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ["admin.alhilalrestaurant.com"], // Add your domain here
+    domains: ["admin.alhilalrestaurant.com"], // Add your domain here
     // domains: ["localhost"], // Add your domain here
-    domains: ["192.168.18.223"], // Add your domain here
+    // domains: ["192.168.18.223"], // Add your domain here
   },
   eslint: {
     ignoreDuringBuilds: true,
